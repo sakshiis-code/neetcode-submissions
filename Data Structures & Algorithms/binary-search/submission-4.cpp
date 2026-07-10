e943fb6afc03fa696000c677cd5926ecf1e05e74
Sakshi Sharma <sakshihihisharma@gmail.com>
2026-07-10 09:42:44 +0530

    Add: binary-search - submission-4

---
+++ Data Structures & Algorithms/binary-search/submission-4.cpp
@@ -0,0 +1,17 @@
+class Solution {
+   public:
+    int search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size();
+
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] >= target) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        return (l < nums.size() && nums[l] == target) ? l : -1;
+    }
+};
